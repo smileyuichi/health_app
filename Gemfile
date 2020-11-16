d16162cde565dce417e91166f62a3a2050b1d7b3
smileyuichi <smile.yuichi.1013@gmail.com>
2020-11-16 19:33:42 +0900

    deviseno

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'devise-i18n'
 # ログイン関連ページにBootstrapを適用するGem
 gem 'devise-bootstrap-views', '~> 1.0'
 
+
 # Gon（コントローラから Javascript に変数を渡せるようにする）
 gem 'gon'
 
@@ -47,6 +48,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'pry-byebug'
   # capistrano
   gem "capistrano", "~> 3.10", require: false
   gem "capistrano-rails", "~> 1.6", require: false
--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require "action_view/railtie"
 # require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-
+require "action_mailer/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -24,6 +24,10 @@ module AwsSampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # 日本語に変更
+    config.i18n.default_locale = :ja
+    # タイムゾーンも変更
+    config.time_zone = 'Asia/Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
